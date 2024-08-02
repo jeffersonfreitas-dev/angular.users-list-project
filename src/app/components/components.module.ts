32351a523f70f4bd80211882e516e4d42f7d08ee
jeffersonfreitas-dev <jefferson.dev21@gmail.com>
2024-08-02 20:59:47 -0300

    componente de filtro

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -1,11 +1,23 @@
 import { NgModule } from "@angular/core";
 import { AngularMaterialModule } from "../angular-material/angular-material.module";
+import { UserDetailsComponent } from './user-details/user-details.component';
+import { FilterComponent } from './filter/filter.component';
+import { FormsModule } from "@angular/forms";
+import { CommonModule } from "@angular/common";
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    UserDetailsComponent,
+    FilterComponent
+  ],
   imports: [
+    FormsModule,
+    CommonModule,
     AngularMaterialModule
   ],
-  exports: []
+  exports: [
+    UserDetailsComponent,
+    FilterComponent
+  ]
 })
 export class ComponentModule {}
